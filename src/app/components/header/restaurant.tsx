b0191f0b777b78e44e9c04d82622a12bb0879015
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-13 00:07:49 +0900

    feat: develop header logic of restaurand and other navbar

--- src/app/components/header/restaurant.tsx
+++ src/app/components/header/restaurant.tsx
@@ -1,5 +1,89 @@
+import {
+    Badge,
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Stack,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
 import React from "react";
 
 export function NavbarRestaurant(props: any) {
-  return <div>NavbarRestaurant</div>;
+    return (
+        <div className="format_restaurant home_navbar">
+            <Container>
+                <Stack
+                    flexDirection={"row"}
+                    className="navbar_config"
+                    justifyContent={"space-between"}
+                >
+                    <Box>
+                        <img src="/icons/Papay.svg" />
+                    </Box>
+                    <Stack
+                        flexDirection={"row"}
+                        justifyContent={"space-evenly"}
+                        alignItems={"center"}
+                        className="navbar_links"
+                    >
+                        <Box className="hover-line" onClick={props.setPath}>
+                            <NavLink to="/">Bosh Sahifa</NavLink>
+                        </Box>
+                        <Box className="hover-line" onClick={props.setPath}>
+                            <NavLink
+                                to="/restaurant"
+                                activeClassName="underline"
+                            >
+                                Oshhona
+                            </NavLink>
+                        </Box>
+                        <Box className="hover-line" onClick={props.setPath}>
+                            <NavLink to="/orders" activeClassName="underline">
+                                Buyurtma
+                            </NavLink>
+                        </Box>
+                        <Box className="hover-line" onClick={props.setPath}>
+                            <NavLink
+                                to="/community"
+                                activeClassName="underline"
+                            >
+                                Jamiyat
+                            </NavLink>
+                        </Box>
+                        <Box className="hover-line" onClick={props.setPath}>
+                            <NavLink to="/help" activeClassName="underline">
+                                Yordam
+                            </NavLink>
+                        </Box>
+                        <Box className="hover-line">
+                            <IconButton
+                                aria-label="cart"
+                                id="basic-button"
+                                aria-controls={undefined}
+                                aria-haspopup="true"
+                                aria-expanded={undefined}
+                                // onClick={handleClick}
+                            >
+                                <Badge badgeContent={3} color="secondary">
+                                    <img src="/icons/shopping_cart.svg" />
+                                </Badge>
+                            </IconButton>
+                        </Box>
+                        <Box>
+                            <Button
+                                variant="contained"
+                                style={{
+                                    color: "#FFFFFF",
+                                    background: "#1976d2",
+                                }}
+                            >
+                                KIRISH
+                            </Button>
+                        </Box>
+                    </Stack>
+                </Stack>
+            </Container>
+        </div>
+    );
 }
