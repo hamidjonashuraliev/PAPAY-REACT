f923fd6f801fb2bcf28c7d1d162903ab820b518d
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-12 23:50:35 +0900

    fix: papay project standards

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,24 +4,7 @@ import "../css/App.css";
 import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-    return (
-        <Container maxWidth="sm">
-            <Stack flexDirection={"column"}>
-                <Box sx={{ my: 4 }}>
-                    <Typography variant="h4" component={"h1"} gutterBottom>
-                        Create React App on TypeScript with REDUX
-                    </Typography>
-                </Box>
-                <Box>
-                    <RippleBadge badgeContent={4}>
-                        <Button color="secondary" variant="contained">
-                            Contained
-                        </Button>
-                    </RippleBadge>
-                </Box>
-            </Stack>
-        </Container>
-    );
+    return <Container></Container>;
 }
 
 export default App;
