5f94c062c68f67e0c896c37e097934f5333f8c3f
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-13 01:03:31 +0900

    fix: restaurant screen develop chosenDish component

--- src/app/screens/RestaurantPage/chosenDish.tsx
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -1,32 +1,31 @@
-import React, { useState } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import Marginer from "../../components/marginer";
 import Button from "@mui/material/Button";
 import Rating from "@mui/material/Rating";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import { FreeMode, Navigation, Thumbs } from "swiper";
-import { Favorite, FavoriteBorder } from "@mui/icons-material";
-import Checkbox from "@mui/material/Checkbox";
 
-const chosen_list = Array.from(Array(3).keys());
+const chosen_list = Array.from(Array(5).keys());
 
 export function ChosenDish() {
     const label = { inputProps: { "aria-label": "checkbox demo" } };
-
     return (
         <div className="chosen_dish_page">
             <Container className="dish_container">
-                <Stack className="chosen_dish_page">
+                <Stack className="chosen_dish_slider">
                     <Swiper
                         className="dish_swiper"
                         loop={true}
                         spaceBetween={10}
-                        // thums={{ swiper: thumbsSwiper }}
+                        navigation={true}
+                        // thumbs={{ swiper: thumbsSwiper }}
                         modules={[FreeMode, Navigation, Thumbs]}
                     >
                         {chosen_list.map((ele) => {
@@ -34,10 +33,32 @@ export function ChosenDish() {
                             return (
                                 <SwiperSlide>
                                     <img
+                                        src={image_path}
                                         style={{
                                             width: "100%",
                                             height: "100%",
                                         }}
+                                    />
+                                </SwiperSlide>
+                            );
+                        })}
+                    </Swiper>
+                    <Swiper
+                        // onSwiper={setThumbsSwiper}
+                        loop={true}
+                        spaceBetween={10}
+                        slidesPerView={3}
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="dish_swiper_2"
+                    >
+                        {chosen_list.map((ele) => {
+                            const image_path = `/others/sandvich.jpg`;
+                            return (
+                                <SwiperSlide>
+                                    <img
+                                        className={"dish_img"}
                                         src={image_path}
                                     />
                                 </SwiperSlide>
@@ -45,6 +66,7 @@ export function ChosenDish() {
                         })}
                     </Swiper>
                 </Stack>
+
                 <Stack className="chosen_dish_info_container">
                     <Box className={"chosen_dish_info_box"}>
                         <strong className={"dish_txt"}>Sweet Sandvich</strong>
@@ -89,7 +111,10 @@ export function ChosenDish() {
                             </div>
                         </Box>
                         <p className={"dish_desc_info"}>
-                            Juda mazzali sandvich
+                            Many desktop publishing packages and web page
+                            editors now use Lorem Ipsum as their default model
+                            text, and a search for 'lorem ipsum' will uncover
+                            many web sites still in their infancy.{" "}
                         </p>
                         <Marginer
                             direction="horizontal"
@@ -102,7 +127,15 @@ export function ChosenDish() {
                             <span>$11</span>
                         </div>
                         <div className={"button_box"}>
-                            <Button variant="contained">
+                            <Button
+                                variant="contained"
+                                style={{
+                                    width: "230px",
+                                    height: "44px",
+                                    borderRadius: "4px",
+                                    fontSize: "15px",
+                                }}
+                            >
                                 Savatga qo'shish
                             </Button>
                         </div>
