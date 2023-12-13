7379e396ac636ba01376e30390047ed73bf09f8e
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-13 11:02:48 +0900

    feat develop page screen and complate papay react project publishing

--- src/app/components/tuiEditor/TuiEditor.tsx
+++ src/app/components/tuiEditor/TuiEditor.tsx
@@ -3,94 +3,102 @@ import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 import {
-  Box,
-  Button,
-  FormControl,
-  MenuItem,
-  Stack,
-  Typography,
-  Select,
-  TextField,
+    Box,
+    Button,
+    FormControl,
+    MenuItem,
+    Stack,
+    Typography,
+    Select,
+    TextField,
 } from "@mui/material";
 
 const TuiEditor = (props: any) => {
-  const editorRef = useRef();
+    const editorRef = useRef();
 
-  return (
-    <Stack>
-      <Stack
-        direction="row"
-        style={{ margin: "40px", justifyContent: "space-evenly" }}
-      >
-        <Box className={"form_row"} style={{ width: "300px" }}>
-          <Typography
-            variant="h3"
-            style={{ margin: "10px", color: "rgb(225 255 233)" }}
-          >
-            Category
-          </Typography>
-          <FormControl sx={{ width: "100%", background: "white" }}>
-            <Select
-              value="celebrity"
-              displayEmpty
-              inputProps={{ "aria-label": "Without label" }}
+    return (
+        <Stack>
+            <Stack
+                direction="row"
+                style={{ margin: "40px", justifyContent: "space-evenly" }}
             >
-              <MenuItem value="">
-                <span>Categoriyani tanlang</span>
-              </MenuItem>
-              <MenuItem value="celebrity">Mashhurlar</MenuItem>
-              <MenuItem value="evaluation">Restaurant baho</MenuItem>
-              <MenuItem value="story">Mening Hikoyam</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Box className="form_row" style={{ width: "300px" }}>
-          <Typography
-            style={{ color: "rgb(225 255 233)", margin: "10px" }}
-            variant="h3"
-          >
-            Mavzu
-          </Typography>
-          <TextField
-            id="filled-basic"
-            label="Mazvu"
-            variant="filled"
-            style={{ width: "300px", background: "white" }}
-          />
-        </Box>
-      </Stack>
-      {/* @ts-ignore */}
-     
-      <Editor
-        ref={editorRef}
-        placeholder="Type here"
-        previewStyle="vertical"
-        height="640px"
-        initialEditType="WYSIWYG"
-        toolbarItems={[
-          ["heading", "bold", "italic", "strike"],
-          ["image", "table", "link"],
-          ["ul", "ol", "task"],
-        ]}
-        hooks={{
-          addImageBlobHook: async (image: any, callback: any) => {
-            return false;
-          },
-          load: function (param: any) {},
-        }}
-      />
-      
-      <Stack direction="row" justifyContent="center">
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ margin: "30px", width: "250px", height: "45px" }}
-        >
-          Register
-        </Button>
-      </Stack>
-    </Stack>
-  );
+                <Box className={"form_row"} style={{ width: "300px" }}>
+                    <Typography
+                        variant="h3"
+                        style={{ margin: "10px", color: "rgb(225 255 233)" }}
+                    >
+                        Category
+                    </Typography>
+                    <FormControl sx={{ width: "100%", background: "white" }}>
+                        <Select
+                            value="celebrity"
+                            displayEmpty
+                            inputProps={{ "aria-label": "Without label" }}
+                        >
+                            <MenuItem value="">
+                                <span>Categoriyani tanlang</span>
+                            </MenuItem>
+                            <MenuItem value="celebrity">Mashhurlar</MenuItem>
+                            <MenuItem value="evaluation">
+                                Restaurant baho
+                            </MenuItem>
+                            <MenuItem value="story">Mening Hikoyam</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box className="form_row" style={{ width: "300px" }}>
+                    <Typography
+                        style={{ color: "rgb(225 255 233)", margin: "10px" }}
+                        variant="h3"
+                    >
+                        Mavzu
+                    </Typography>
+                    <TextField
+                        id="filled-basic"
+                        label="Mazvu"
+                        variant="filled"
+                        style={{ width: "300px", background: "white" }}
+                    />
+                </Box>
+            </Stack>
+
+            {/* @ts-ignore */}
+            <Editor
+                ref={editorRef}
+                initialValue="Type here"
+                placeholder="Type here"
+                previewStyle="vertical"
+                height="640px"
+                initialEditType="WYSIWYG"
+                toolbarItems={[
+                    ["heading", "bold", "italic", "strike"],
+                    ["image", "table", "link"],
+                    ["ul", "ol", "task"],
+                ]}
+                // hooks={{
+                //   addImageBlobHook: async (image: any, callback: any) => {
+                //     const uploadImageURL = await uploadImage(image);
+                //     console.log("uploadImageURL:", uploadImageURL);
+                //     callback(uploadImageURL);
+                //     return false;
+                //   },
+                // }}
+                // events={{
+                //   load: function (param: any) {},
+                // }}
+            />
+
+            <Stack direction="row" justifyContent="center">
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: "30px", width: "250px", height: "45px" }}
+                >
+                    Register
+                </Button>
+            </Stack>
+        </Stack>
+    );
 };
 
 export default TuiEditor;
--- src/app/screens/HelpPage/index.tsx
+++ src/app/screens/HelpPage/index.tsx
@@ -1,6 +1,232 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack, Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Tab from "@mui/material/Tab";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import "../../../css/help.css";
 
 export function HelpPage() {
-  return <Container>HelpPage</Container>;
+    /** INITIALIZATIONS **/
+    const [value, setValue] = React.useState("1");
+    const faq = [
+        {
+            question: "To'lov qanday amalga oshiriladi?",
+            answer: "To'lovni Payme, click ilovalari orqali amalga oshiring",
+        },
+        {
+            question: "Buyurtmalar qanday vaqtda yetib keladi?",
+            answer: "Buyurtmalar harid qilgan narsangizga qarab har xil vaqtda yetkazilishi mumkin. Maximum 1 soat ichida!",
+        },
+        {
+            question:
+                "saytdan foydalansam ma'lumotlarim havsizligiga kafolat bormi?",
+            answer: "albatta, bizning dasturchilamiz sizning ma'lumotlaringiz havfsizligiga kafolat berishadi.",
+        },
+        {
+            question: "saytda muommo yuzaga kelsa kimga murojat qilaman?",
+            answer: "Hurmatli mijoz, iltimos adminga xat yo'llash bo'limidan foydalaning",
+        },
+        {
+            question:
+                "Men Koreya davlatidaman. O'zbekistondagi oilam uchun Ovqat buyurtma qilsam bo'ladimi?",
+            answer: "Albatta, chet eldan turib nafaqat visa va master balkim paypal orqali ham to'lovni amalga oshirishingiz mumkin.",
+        },
+        {
+            question:
+                "buyurtmani bekor qilmoqchiman lekin buni qanday qilishni bilmayman?",
+            answer: "Buyurtmani bekor qilish uchun Avvalo login qiling va buyurtmalarim bo'limidan kerakli bo'lgan buyurtmangizni bekor qilish tugmasi orqali bekor qilsangiz bo'ladi!",
+        },
+        {
+            question: "Buyurmani To'lov qilish uchun nima qilishim kerak?",
+            answer: "Buyurtma uchun to'lovni amalga oshirish uchun login qilishingiz va buyurtmalarim sahifasiga o'tishingiz lozim. Ochilgan sahifadan to'lov qilish tugmasi orqali to'lovni amalga oshira olasiz",
+        },
+        {
+            question: "karta ma'lumotlarimni qayerga kiritaman?",
+            answer: "Karta ma'lumotlarini Buyurtmalarim sahifasining o'ng tomonida joy bor",
+        },
+        {
+            question:
+                "Buyurtmani yakunlagandan so'ng o'zimni fikrimni yozib qoldirishim kerakmi?",
+            answer: "Hurmatli mijoz, sizni fikr qoldirishga majburlamaymiz, lekin fikringiz biz uchun juda qadrli hisoblanadi!",
+        },
+        {
+            question: "Maqola yozishni xohlayman",
+            answer: "Maqola yozish uchun sahifa bo'limidan maqola yozish tugmasini bosishingiz bossangiz kifoya",
+        },
+        {
+            question: "Jonli muloqatga men ham qatnasha olamanmi?",
+            answer: "albatta buning uchun saytimizdan ro'yhatdan o'ting va Jamiyat bo'limidan bemalol foydalanishingiz va o'z fikrlaringizni yozib qoldirishingiz mumkin!",
+        },
+        {
+            question:
+                "Birorbir sahifani ko'p kuzatsam uni o'zimni sahifamdan tezda topib olish uchun nima qilishim kerak??",
+            answer: "Buning uchun siz kuzatmoqchi bo'lgan foydalanuvchingizda follow bo'lishingiz kifoya qiladi!",
+        },
+        {
+            question: "Sayt rivoji uchun o'z hissamni qo'shmoqchiman",
+            answer: "Albatta buning uchun adminga xat qoldirishingiz yoki berilgan telefon no'merlarga murojat qilishiz so'raladi.",
+        },
+    ];
+
+    const rules = [
+        `Saytdan to'laqonli yani buyurtmalar qilish, jonli
+    muloqotlardan foydalanishingiz uchun ro'yxatdan
+    o'tishingiz shart.`,
+        `Buyurtmalaringizga to'lovni amalga oshirganingizdan so'ng 
+    bekor qilishning imkoni yo'q shu sababli to'lovlarni 
+    amalga oshirishdan avval tekshirib oling.`,
+        `Jonli muloqot vaqtida bexayo so'zlarni ishlatish mutlaqo
+    taqiqlanadi.`,
+        `Shaxsiy reklamalarni adminning ruxsatisiz yozish va 
+    tarqatish mumkin emas.`,
+        `Maqolalaringiz odob doirasidan chiqib ketmasligi shart.`,
+        `Barcha xarakatlaringiz adminlarimiz nazorati ostida
+    bo'lgani sabab iltimos talablarimizni xurmat qiling.`,
+    ];
+
+    /** HANDLERS **/
+    const handleChange = (event: any, newValue: string) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className="help_page">
+            <Container sx={{ mt: "50px", mb: "50px" }}>
+                <TabContext value={value}>
+                    <Box className={"help_menu"}>
+                        <Box
+                            className={"help_menu_main"}
+                            sx={{ borderBottom: 1, borderColor: "divider" }}
+                        >
+                            {/* <TabList
+                value={value}
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+                // style={{ display: "flex", justifyContent: "space-between" }}
+              > */}
+                            <Tab
+                                label="Qoidalar"
+                                value={"1"}
+                                style={{ marginRight: "20rem" }}
+                            />
+                            <Tab
+                                label="FAQ"
+                                value={"2"}
+                                style={{ marginRight: "20rem" }}
+                            />
+                            <Tab label="Adminga xat" value={"3"} />
+                            {/* </TabList> */}
+                        </Box>
+                    </Box>
+                    <Box
+                        sx={{
+                            borderBottom: 1,
+                            borderColor: "#fff",
+                            mt: "15px",
+                        }}
+                    ></Box>
+                    <Stack>
+                        <Stack className={"help_main_content"}>
+                            <TabPanel value={"1"}>
+                                <Stack className="theRules_box">
+                                    <Box className={"theRulesFrame"}>
+                                        {rules.map((ele, number) => {
+                                            return <p>{ele}</p>;
+                                        })}
+                                    </Box>
+                                </Stack>
+                            </TabPanel>
+                            <TabPanel value={"2"}>
+                                <Stack className={"accordion_menu"}>
+                                    {faq.map((ele, number) => {
+                                        return (
+                                            <Accordion>
+                                                <AccordionSummary
+                                                    expandIcon={
+                                                        <ExpandMoreIcon />
+                                                    }
+                                                    aria-controls="panella-content"
+                                                    id="panella-header"
+                                                >
+                                                    <Typography>
+                                                        {ele.question}
+                                                    </Typography>
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    <Typography>
+                                                        {ele.answer}
+                                                    </Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                        );
+                                    })}
+                                </Stack>
+                            </TabPanel>
+                            <TabPanel value={"3"}>
+                                <Stack className={"admin_letter_box"}>
+                                    <Stack className={"admin_letter_container"}>
+                                        <Box className={"admin_letter_frame"}>
+                                            <span>Adminga Xabar Qoldirish</span>
+                                            <p>
+                                                Assalomu alaykum! Adminga xabar
+                                                qoldirish uchun pasdagi
+                                                formlarni to’ldiring!{" "}
+                                            </p>
+                                        </Box>
+                                        <form
+                                            action={"#"}
+                                            method={"POST"}
+                                            className={"admin_letter_frame"}
+                                        >
+                                            <div className="admin_input_box">
+                                                <label>Ism</label>
+                                                <input
+                                                    type="text"
+                                                    name="mb_nick"
+                                                    placeholder="Ism"
+                                                />
+                                            </div>
+                                            <div className="admin_input_box">
+                                                <label>Elektron Manzil</label>
+                                                <input
+                                                    type="text"
+                                                    name="ma_email"
+                                                    placeholder="Elektron Manzil"
+                                                />
+                                            </div>
+                                            <div className="admin_input_box">
+                                                <label>Xabar</label>
+                                                <textarea
+                                                    name="mb_msg"
+                                                    placeholder="Xabar"
+                                                ></textarea>
+                                            </div>
+                                            <Box
+                                                display={"flex"}
+                                                justifyContent={"flex-end"}
+                                                sx={{ mt: "30px" }}
+                                            >
+                                                <Button
+                                                    type={"submit"}
+                                                    variant="contained"
+                                                >
+                                                    Jo'natish
+                                                </Button>
+                                            </Box>
+                                        </form>
+                                    </Stack>
+                                </Stack>
+                            </TabPanel>
+                        </Stack>
+                    </Stack>
+                </TabContext>
+            </Container>
+        </div>
+    );
 }
