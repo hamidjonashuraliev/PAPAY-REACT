ca9ced0edac5759a3cf54f82753de4adc6d53350
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-12-21 13:53:12 +0900

    feat: practice useState hooks on functional component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -53,8 +53,8 @@ function App() {
                 </Route>
                 <Route path="/">
 
-                    <Car />
-                    {/* <HomePage /> */}
+                    {/* <Car /> */}
+                    <HomePage />
                 </Route>
             </Switch>
 
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,10 +6,14 @@ import {
   IconButton,
   Stack,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export function NavbarHome(props: any) {
+// INITIALIZATIONS
+const  [count, setCount] = useState(0);
+
+
   return (
     <div className="format home_navbar">
       <Container>
@@ -88,7 +92,8 @@ export function NavbarHome(props: any) {
             <Box className="define_restaurant">
               The Authentic Restaurant & Cafe
             </Box>
-            <Box className="timeline_service">24 soat xizmatingizdamiz.</Box>
+            <Box className="timeline_service">
+              {count} soat xizmatingizdamiz.</Box>
             <Box sx={{ mt: "90px" }}>
               <Button
                 variant="contained"
@@ -98,6 +103,8 @@ export function NavbarHome(props: any) {
                   background: "#1976d2",
                   color: "#FFFFFF",
                 }}
+
+                onClick={() => setCount(count + 1)}
               >
                 RO’YHATDAN O’TISH
               </Button>
