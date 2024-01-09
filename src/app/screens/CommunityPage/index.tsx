02131a86a9546e9e59a06181a8aa22dffe0393d8
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2024-01-09 21:47:28 +0900

    fix: modify communityPage like chosen articles by authenicated members

--- src/app/screens/CommunityPage/index.tsx
+++ src/app/screens/CommunityPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Container, Stack } from "@mui/material";
 import Tab from "@mui/material/Tab";
 import Pagination from "@mui/material/Pagination";
@@ -36,99 +36,131 @@ const targetBoArticlesRetriever = createSelector(
 
 
 export function CommunityPage(props: any) {
-   /** INITIALIZATIONS */
+    // INITIALIZATIONS
+    const { setTargetBoArticles } = actionDispatch(useDispatch());
+    const { targetBoArticles } = useSelector(targetBoArticlesRetriever);
+  
     const [value, setValue] = React.useState("1");
-
-    // HANDLERS
+    const [searchArticlesObj, setSearchArticlesObj] = useState<SearchArticlesObj>(
+      {
+        bo_id: "all",
+        page: 1,
+        limit: 5,
+      }
+    );
+    const [articlesRebuild, setArticlesRebuild] = useState<Date>(new Date());
+  
+    useEffect(() => {
+      const communityService = new CommunityApiService();
+      communityService
+        .getTargetArticles(searchArticlesObj)
+        .then((data) => setTargetBoArticles(data))
+        .catch((err) => console.log(err));
+    }, [searchArticlesObj, articlesRebuild]);
+  
+    /** HANDLERS **/
     const handleChange = (event: any, newValue: string) => {
-        setValue(newValue);
+      searchArticlesObj.page = 1;
+      switch (newValue) {
+        case "1":
+          searchArticlesObj.bo_id = "all";
+          break;
+        case "2":
+          searchArticlesObj.bo_id = "celebrity";
+          break;
+        case "3":
+          searchArticlesObj.bo_id = "evaluation";
+          break;
+        case "4":
+          searchArticlesObj.bo_id = "story";
+          break;
+      }
+      setSearchArticlesObj({ ...searchArticlesObj });
+      setValue(newValue);
     };
-
-    const handlePaginationChange = (event: any, newValue: number) => {
-        console.log(value);
+  
+    const handlePaginationChange = (event: any, value: number) => {
+      searchArticlesObj.page = value;
+      setSearchArticlesObj({ ...searchArticlesObj });
     };
-
+  
     return (
-        <div className="community_page">
-            <div className="community_frame">
-                <Container sx={{ mt: "50px", mb: "50px" }}>
-                    <Stack flexDirection="row" justifyContent="space-between">
-                        <CommunityChats />
-                        <Stack
-                            className="community_all_frame"
-                            inputMode="text"
-                            style={{ border: "1px solid #fff" }}
-                        >
-                            <TabContext value={value}>
-                                <Box className="article_tabs">
-                                    <Box
-                                        sx={{
-                                            borderBottom: 1,
-                                            borderColor: "divider",
-                                        }}
-                                    >
-                                        {/* <TabList
-                      value={value}
-                      onChange={handleChange}
-                      aria-label="lab API tabs example"
-                      style={{ borderColor: "blue" }}
-                    > */}
-                                        <Tab
-                                            label="Barcha Maqolalar"
-                                            value="1"
-                                        />
-                                        <Tab label="Mashxurlar" value="2" />
-                                        <Tab label="Oshxonaga baxo" value="3" />
-                                        <Tab label="Hikoyalar" value="4" />
-                                        {/* </TabList> */}
-                                    </Box>
-                                </Box>
-
-                                <Box className="article_main">
-                                    <TabPanel value="1">
-                                        <TargetArticles
-                                            targetBoArticles={[TargetArticles]}
-                                        />
-                                    </TabPanel>
-                                    <TabPanel value="2">
-                                        <TargetArticles
-                                            targetBoArticles={[TargetArticles]}
-                                        />
-                                    </TabPanel>
-                                    <TabPanel value="3">
-                                        <TargetArticles
-                                          targetBoArticles={[TargetArticles]}
-                                        />
-                                    </TabPanel>
-                                    <TabPanel value="4">
-                                        <TargetArticles
-                                            targetBoArticles={[TargetArticles]}
-                                        />
-                                    </TabPanel>
-                                </Box>
-
-                                <Box className="article_bott">
-                                    <Pagination
-                                        count={5}
-                                        page={1}
-                                        renderItem={(item) => (
-                                            <PaginationItem
-                                                components={{
-                                                    previous: ArrowBackIcon,
-                                                    next: ArrowForwardIcon,
-                                                }}
-                                                {...item}
-                                                color={"secondary"}
-                                            />
-                                        )}
-                                        onChange={handlePaginationChange}
-                                    />
-                                </Box>
-                            </TabContext>
-                        </Stack>
-                    </Stack>
-                </Container>
-            </div>
+      <div className="community_page">
+        <div className="community_frame">
+          <Container sx={{ mt: "50px", mb: "50px" }}>
+            <Stack flexDirection="row" justifyContent="space-between">
+              <CommunityChats />
+              <Stack
+                className="community_all_frame"
+                inputMode="text"
+                style={{ border: "1px solid #fff" }}
+              >
+                <TabContext value={value}>
+                  <Box className="article_tabs">
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                      <TabList
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="lab API tabs example"
+                        style={{ borderColor: "blue" }}
+                      >
+                        <Tab label="Barcha Maqolalar" value="1" />
+                        <Tab label="Mashxurlar" value="2" />
+                        <Tab label="Oshxonaga baxo" value="3" />
+                        <Tab label="Hikoyalar" value="4" />
+                      </TabList>
+                    </Box>
+                  </Box>
+  
+                  <Box className="article_main">
+                    <TabPanel value="1">
+                      <TargetArticles
+                        targetBoArticles={targetBoArticles}
+                        setArticlesRebuild={setArticlesRebuild}
+                      />
+                    </TabPanel>
+                    <TabPanel value="2">
+                      <TargetArticles
+                        targetBoArticles={targetBoArticles}
+                        setArticlesRebuild={setArticlesRebuild}
+                      />
+                    </TabPanel>
+                    <TabPanel value="3">
+                      <TargetArticles
+                        targetBoArticles={targetBoArticles}
+                        setArticlesRebuild={setArticlesRebuild}
+                      />
+                    </TabPanel>
+                    <TabPanel value="4">
+                      <TargetArticles
+                        targetBoArticles={targetBoArticles}
+                        setArticlesRebuild={setArticlesRebuild}
+                      />
+                    </TabPanel>
+                  </Box>
+  
+                  <Box className="article_bott">
+                    <Pagination
+                      count={5}
+                      page={1}
+                      renderItem={(item) => (
+                        <PaginationItem
+                          components={{
+                            previous: ArrowBackIcon,
+                            next: ArrowForwardIcon,
+                          }}
+                          {...item}
+                          color={"secondary"}
+                        />
+                      )}
+                      onChange={handlePaginationChange}
+                    />
+                  </Box>
+                </TabContext>
+              </Stack>
+            </Stack>
+          </Container>
         </div>
+      </div>
     );
 }
--- src/app/screens/CommunityPage/targetArticles.tsx
+++ src/app/screens/CommunityPage/targetArticles.tsx
@@ -5,6 +5,13 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { boArticle } from "../../../types/boArticle";
 import { serverApi } from "../../../lib/config";
+import {
+    sweetErrorHandling,
+    sweetTopSmallSuccessAlert,
+  } from "../../../lib/sweetAlert";
+  import assert from "assert";
+  import { Definer } from "../../../lib/Definer";
+  import MemberAipService from "../../apiServices/memberApiService";
 import Checkbox from "@mui/material/Checkbox";
 import moment from "moment";
 
@@ -15,6 +22,26 @@ const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export function TargetArticles(props: any) {
     const time = moment().format("YY-MM-DD HH:mm");
+    const {setArticlesRebuild} = props
+    /** HANDLERS **/
+    const targetLikeHandler = async (e: any) => {
+      try {
+        assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
+  
+        const memberService = new MemberAipService();
+        const like_result = await memberService.memberLikeTarget({
+          like_ref_id: e.target.id,
+          group_type: "community",
+        });
+        assert.ok(like_result, Definer.general_err1);
+        await sweetTopSmallSuccessAlert("success", 800, false);
+        setArticlesRebuild(new Date())
+  
+      } catch (err: any) {
+        console.log(err);
+        sweetErrorHandling(err).then();
+      }
+    };
 
     return (
         <Stack>
@@ -45,13 +72,15 @@ export function TargetArticles(props: any) {
                   </Box>
                   <Box display="flex" flexDirection="column" sx={{ mt: "15px" }}>
                     <span className={"all_article_title"}>{article?.bo_id}</span>
-                    <span className={"all_article_desc"}>{article?.art_subject}</span>
+                    <span className={"all_article_desc"}>
+                      {article?.art_subject}
+                    </span>
                   </Box>
                   <Box
                     display={"flex"}
                     alignItems={"center"}
-                    justifyContent={'flex-end'}
-                    color={'#fff'}
+                    justifyContent={"flex-end"}
+                    color={"#fff"}
                     className={"target_icons"}
                   >
                     <span>{moment().format("YY-MM-DD HH:mm")}</span>
@@ -60,12 +89,18 @@ export function TargetArticles(props: any) {
                       icon={<FavoriteBorder />}
                       checkedIcon={<Favorite style={{ color: "red" }} />}
                       id={article?._id}
-                      /*@ts-ignore */
-                      checked={false}
+                      onClick={targetLikeHandler}
+                      checked={
+                        article?.me_liked && article.me_liked[0]?.my_favorite
+                          ? true
+                          : false
+                      }
                     />
                     <span style={{ marginRight: "18px" }}>{article.art_likes}</span>
                     <RemoveRedEyeIcon style={{ color: "white" }} />
-                    <span style={{ marginRight: "18px", marginLeft: '10px' }}>{article.art_views}</span>
+                    <span style={{ marginRight: "18px", marginLeft: "10px" }}>
+                      {article.art_views}
+                    </span>
                   </Box>
                 </Box>
               </Link>
