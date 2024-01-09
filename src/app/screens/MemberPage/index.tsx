51971d9e32fd11dcd56dc5c45d5be28395cd1478
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2024-01-09 22:45:45 +0900

    feat: organize retrieve of memberPage url query data

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import { Container } from "@mui/material";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch, useLocation } from "react-router-dom";
 import { VisitOtherPage } from "./VisitOtherPage";
 import { VisitMyPage } from "./VisitMyPage";
 import "../../../css/my_page.css";
 
+function useQuery() {
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
 export function MemberPage(props: any) {
-    const { verifiedMemberData } = props;
+    const { verifieaMemberData } = props;
+    const query = useQuery();
     let member = useRouteMatch();
-    console.log(member);
+    const chosen_mb_id: string | null = query.get("mb_id") ?? null;
+    const chosen_art_id: string | null = query.get("art_id") ?? null;
+
+    console.log("QUERY TEST", query.get("art_id"));
 
     return (
         <div className="restaurant_page">
             <Switch>
                 <Route path={`${member.path}/other`}>
-                    <VisitOtherPage verifiedMemberData={verifiedMemberData} />
+                    <VisitOtherPage
+                        verifieaMemberData={verifieaMemberData}
+                        chosen_mb_id={chosen_mb_id}
+                        chosen_art_id={chosen_art_id}
+                    />
                 </Route>
 
                 <Route path={`${member.path}`}>
-                    <VisitMyPage verifiedMemberData={verifiedMemberData} />
+                    <VisitMyPage verifieaMemberData={verifieaMemberData} />
                 </Route>
             </Switch>
         </div>
